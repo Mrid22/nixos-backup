c12567269b8470b3d586ad5bdef1a7c2ccc5881b
Mrid22 <mridulaga@outlook.com>
2025-04-07 22:13:42 +0800

    Allowed-Spaces-in-Commit-msg

--- reload.nix
+++ reload.nix
@@ -4,7 +4,7 @@ pkgs.writeShellScriptBin "reload" ''
     echo "Syncing with git"
     read -p 'Commit Message: ' commitmsg
     ${pkgs.git}/bin/git add .
-    ${pkgs.git}/bin/git commit -m $commitmsg
+    ${pkgs.git}/bin/git commit -m "$commitmsg"
     ${pkgs.git}/bin/git push
     sudo nixos-rebuild switch --flake /etc/nixos/nixos-backup/.
 ''
