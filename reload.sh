adbfa6b07ae80283620a4552971380d778ce826b
Mrid22 <mridulaga@outlook.com>
2025-04-07 21:21:55 +0800

    removed bash script

--- reload.sh
+++
@@ -1,9 +0,0 @@
-echo "Syncing with git"
-echo -n "Commit Message: "
-read -r commit_message
-git add .
-git commit -m commit_message
-git push
-echo "reloading config"
-sudo nixos-rebuild switch --flake /etc/nixos/nixos-backup/.
-echo "Done"
