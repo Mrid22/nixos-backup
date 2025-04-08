22d45b87dc1bbb4f1cb3bf10421ee913998d9d16
Mrid22 <mridulaga@outlook.com>
2025-04-08 18:58:13 +0800

    reload.nix - CHanged it to work globally

--- reload.nix
+++ reload.nix
@@ -3,9 +3,10 @@
 pkgs.writeShellScriptBin "reload" ''
     echo "Syncing with git"
     ${pkgs.git}/bin/git pull
+    PATH = /etc/nixos/nixos-backup/.
     read -p 'Commit Message: ' commitmsg
-    ${pkgs.git}/bin/git add .
-    ${pkgs.git}/bin/git commit -m "$commitmsg"
-    ${pkgs.git}/bin/git push
-    sudo nixos-rebuild switch --flake /etc/nixos/nixos-backup/.
+    ${pkgs.git}/bin/git -C $PATH add .
+    ${pkgs.git}/bin/git -C $PATH commit -m "$commitmsg"
+    ${pkgs.git}/bin/git -C $PATH push
+    sudo nixos-rebuild switch --flake $PATH
 ''
