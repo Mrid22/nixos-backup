3376b570d76cd2ff668426942aae91256b402973
Mrid22 <153362027+Mrid22@users.noreply.github.com>
2025-04-10 05:12:57 +0000

    formating changes

--- home.nix
+++ home.nix
@@ -35,30 +35,30 @@
       };
       animations = {
         enabled = "yes, please :)";
-	bezier = [
-	  "easeOutQuint,0.23,1,0.32,1"
-	  "easeInOutCubic,0.65,0.05,0.36,1"
-	  "linear,0,0,1,1"
-	  "almostLinear,0.5,0.5,0.75,1.0"
-	  "quick,0.15,0,0.1,1"
-	];
+	      bezier = [
+	        "easeOutQuint,0.23,1,0.32,1"
+	        "easeInOutCubic,0.65,0.05,0.36,1"
+	        "linear,0,0,1,1"
+	        "almostLinear,0.5,0.5,0.75,1.0"
+	        "quick,0.15,0,0.1,1"
+	      ];
         animation = [
-        "global, 1, 10, default"
-	  "windows, 1, 4.79, easeOutQuint"
-	  "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
-	  "windowsOut, 1, 1.49, linear, popin 87%"
+          "global, 1, 10, default"
+	        "windows, 1, 4.79, easeOutQuint"
+	        "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
+	        "windowsOut, 1, 1.49, linear, popin 87%"
           "fadeIn, 1, 1.73, almostLinear"
-	  "fadeOut, 1, 1.46, almostLinear"
-	  "fade, 1, 3.03, quick"
-	  "layers, 1, 3.81, easeOutQuint"
-	  "layersIn, 1, 4, easeOutQuint, fade"
-	  "layersOut, 1, 1.5, linear, fade"
-	  "fadeLayersIn, 1, 1.79, almostLinear"
-	  "fadeLayersOut, 1, 1.39, almostLinear"
-	  "workspaces, 1, 1.94, almostLinear, slide"
-	  "workspacesIn, 1, 1.21, almostLinear, slide"
-	  "workspacesOut, 1, 1.94, almostLinear, slide"
-	];
+	        "fadeOut, 1, 1.46, almostLinear"
+	        "fade, 1, 3.03, quick"
+	        "layers, 1, 3.81, easeOutQuint"
+	        "layersIn, 1, 4, easeOutQuint, fade"
+	        "layersOut, 1, 1.5, linear, fade"
+	        "fadeLayersIn, 1, 1.79, almostLinear"
+	        "fadeLayersOut, 1, 1.39, almostLinear"
+	        "workspaces, 1, 1.94, almostLinear, slide"
+	        "workspacesIn, 1, 1.21, almostLinear, slide"
+	        "workspacesOut, 1, 1.94, almostLinear, slide"
+	      ];
       };
       "$mod" = "ALT";
       bind = [
@@ -75,7 +75,7 @@
         "$mod, K, movefocus, u"
         "$mod, L, movefocus, r"
         
-	# Workspaces
+	      # Workspaces
         "$mod, 1, workspace, 1"
         "$mod, 2, workspace, 2"
         "$mod, 3, workspace, 3"
@@ -86,7 +86,7 @@
         "$mod, 8, workspace, 8"
         "$mod, 9, workspace, 9"
         "$mod, 0, workspace, 10"
- 
+        
         "$mod SHIFT, 1, movetoworkspace, 1"
         "$mod SHIFT, 2, movetoworkspace, 2"
         "$mod SHIFT, 3, movetoworkspace, 3"
