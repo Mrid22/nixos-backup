f72be26c25a6baf0fe0a86d091e34c7ee40dd435
Mrid22 <mridulaga@outlook.com>
2025-04-11 18:48:15 +0800

    nvf - configured debugger

--- home.nix
+++ home.nix
@@ -1,6 +1,8 @@
-{ pkgs, inputs, ... }:
-
 {
+  pkgs,
+  inputs,
+  ...
+}: {
   imports = with inputs; [
     gauntlet.homeManagerModules.default
     nvf.homeManagerModules.default
@@ -36,30 +38,30 @@
       };
       animations = {
         enabled = "yes, please :)";
-	      bezier = [
-	        "easeOutQuint,0.23,1,0.32,1"
-	        "easeInOutCubic,0.65,0.05,0.36,1"
-	        "linear,0,0,1,1"
-	        "almostLinear,0.5,0.5,0.75,1.0"
-	        "quick,0.15,0,0.1,1"
-	      ];
+        bezier = [
+          "easeOutQuint,0.23,1,0.32,1"
+          "easeInOutCubic,0.65,0.05,0.36,1"
+          "linear,0,0,1,1"
+          "almostLinear,0.5,0.5,0.75,1.0"
+          "quick,0.15,0,0.1,1"
+        ];
         animation = [
           "global, 1, 10, default"
-	        "windows, 1, 4.79, easeOutQuint"
-	        "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
-	        "windowsOut, 1, 1.49, linear, popin 87%"
+          "windows, 1, 4.79, easeOutQuint"
+          "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
+          "windowsOut, 1, 1.49, linear, popin 87%"
           "fadeIn, 1, 1.73, almostLinear"
-	        "fadeOut, 1, 1.46, almostLinear"
-	        "fade, 1, 3.03, quick"
-	        "layers, 1, 3.81, easeOutQuint"
-	        "layersIn, 1, 4, easeOutQuint, fade"
-	        "layersOut, 1, 1.5, linear, fade"
-	        "fadeLayersIn, 1, 1.79, almostLinear"
-	        "fadeLayersOut, 1, 1.39, almostLinear"
-	        "workspaces, 1, 1.94, almostLinear, slide"
-	        "workspacesIn, 1, 1.21, almostLinear, slide"
-	        "workspacesOut, 1, 1.94, almostLinear, slide"
-	      ];
+          "fadeOut, 1, 1.46, almostLinear"
+          "fade, 1, 3.03, quick"
+          "layers, 1, 3.81, easeOutQuint"
+          "layersIn, 1, 4, easeOutQuint, fade"
+          "layersOut, 1, 1.5, linear, fade"
+          "fadeLayersIn, 1, 1.79, almostLinear"
+          "fadeLayersOut, 1, 1.39, almostLinear"
+          "workspaces, 1, 1.94, almostLinear, slide"
+          "workspacesIn, 1, 1.21, almostLinear, slide"
+          "workspacesOut, 1, 1.94, almostLinear, slide"
+        ];
       };
       "$mod" = "ALT";
       bind = [
@@ -75,8 +77,8 @@
         "$mod, J, movefocus, d"
         "$mod, K, movefocus, u"
         "$mod, L, movefocus, r"
-        
-	      # Workspaces
+
+        # Workspaces
         "$mod, 1, workspace, 1"
         "$mod, 2, workspace, 2"
         "$mod, 3, workspace, 3"
@@ -87,7 +89,7 @@
         "$mod, 8, workspace, 8"
         "$mod, 9, workspace, 9"
         "$mod, 0, workspace, 10"
-        
+
         "$mod SHIFT, 1, movetoworkspace, 1"
         "$mod SHIFT, 2, movetoworkspace, 2"
         "$mod SHIFT, 3, movetoworkspace, 3"
@@ -157,7 +159,7 @@
     nvf = {
       enable = true;
       settings = {
-        vim  = {
+        vim = {
           viAlias = true;
           vimAlias = true;
           statusline.lualine.enable = true;
@@ -169,6 +171,12 @@
           };
           lsp = {
             formatOnSave = true;
+          };
+          debugger = {
+            nvim-dap = {
+              enable = true;
+              ui.enable = true;
+            };
           };
           languages = {
             enableLSP = true;
@@ -186,7 +194,7 @@
       };
     };
   };
- 
+
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
