24e1e181cc416c559f37337134030e75d8c1a64c
Mrid22 <mridulaga@outlook.com>
2025-04-14 13:08:06 +0800

    added update script

--- update.nix
+++ update.nix
@@ -3,10 +3,8 @@ pkgs.writeShellScriptBin "update" ''
   path=/etc/nixos/nixos-backup/.
   echo "Syncing with git"
   ${pkgs.git}/bin/git -C $path pull
-  commitmsg = "system update"
   ${pkgs.git}/bin/git -C $path add .
-  ${pkgs.git}/bin/git -C $path commit -m "$commitmsg"
-  nix flake update
-  sudo nixos-rebuild switch --flake $path
+  ${pkgs.git}/bin/git -C $path commit -m "update"
   ${pkgs.git}/bin/git -C $path push
+  sudo nixos-rebuild switch --flake $path
 ''
