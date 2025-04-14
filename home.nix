f3e84be04e209ea0fd9b6df3f587ac2b821a4a7c
Mrid22 <mridulaga@outlook.com>
2025-04-14 15:12:20 +0800

    waybar - added workspaces

--- home.nix
+++ home.nix
@@ -162,29 +162,11 @@
           layer = "top";
           position = "top";
           height = 30;
-          output = [
-            "eDP-1"
-            "HDMI-A-1"
-          ];
-          modules-left = ["sway/workspaces" "sway/mode" "wlr/taskbar"];
-          modules-center = ["sway/window" "custom/hello-from-waybar"];
-          modules-right = ["mpd" "custom/mymodule#with-css-id" "temperature"];
-
-          "sway/workspaces" = {
-            disable-scroll = true;
-            all-outputs = true;
-          };
-          "custom/hello-from-waybar" = {
-            format = "hello {}";
-            max-length = 40;
-            interval = "once";
-            exec = pkgs.writeShellScript "hello-from-waybar" ''
-              echo "from within waybar"
-            '';
-          };
+          modules-left = ["hyprland/workspaces"];
+          modules-center = [];
+          modules-right = [];
         };
       };
-
       style = {};
     };
     lazygit = {
