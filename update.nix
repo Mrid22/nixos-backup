3c376fed83029dce8bf84f2714add6463e16a495
Mrid22 <mridulaga@outlook.com>
2025-04-14 13:24:08 +0800

    update

--- update.nix
+++ update.nix
@@ -1,10 +1,10 @@
 {pkgs}:
 pkgs.writeShellScriptBin "update" ''
   path=/etc/nixos/nixos-backup/.
-  echo "Syncing with git"
-  ${pkgs.git}/bin/git -C $path pull
+  nix flake update
   ${pkgs.git}/bin/git -C $path add .
   ${pkgs.git}/bin/git -C $path commit -m "update"
   ${pkgs.git}/bin/git -C $path push
-  sudo nixos-rebuild switch --flake $path
+  sudo nixos-rebuild switch --recreate-lock-file --flake $path
+
 ''
