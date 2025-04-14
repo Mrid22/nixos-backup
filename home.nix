e9d67faa81539e7f1bd4cabb1aa866ef556d066e
Mrid22 <mridulaga@outlook.com>
2025-04-14 15:36:47 +0800

    waybar - turned settigns to a list

--- home.nix
+++ home.nix
@@ -157,16 +157,7 @@
     };
     waybar = {
       enable = true;
-      settings = {
-        mainBar = {
-          layer = "top";
-          position = "top";
-          height = 30;
-          modules-left = ["hyprland/workspaces"];
-          modules-center = [];
-          modules-right = [];
-        };
-      };
+      settings = [];
       style = {};
     };
     lazygit = {
