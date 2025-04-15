42895cdea1eae4cd68685e044bab86f86e791072
Mrid22 <mridulaga@outlook.com>
2025-04-16 03:53:30 +0800

    hyprland: configured brightness and volume keys

--- home.nix
+++ home.nix
@@ -111,6 +111,10 @@
         # Volume and Brightness
         ",XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
         ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+        ",XF86MonBrightnessUp, exec, brightnessctl s 10%+"
+        ",XF86MonBrightnessDown, exec, brightnessctl s 10%-"
       ];
     };
   };
