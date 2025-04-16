485528f951ff74e365bc406c63048cfb0b6581f4
Mrid22 <mridulaga@outlook.com>
2025-04-16 16:14:48 +0800

    typo

--- home.nix
+++ home.nix
@@ -201,7 +201,7 @@
           path = "wallpaper.jpg";
         };
         input-field = {
-          size = 300, 60;
+          size = "300, 60";
           outline_thickness = 0;
           inner_color = "rgba(0,0,0,0.5)";
           fade_on_empty = false;
