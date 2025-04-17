ed2bd709fecd6ebe6aa5d8167232c3a036f1e110
Mrid22 <mridulaga@outlook.com>
2025-04-17 20:41:42 +0800

    cleaned up autogit script

--- autogit.nix
+++ autogit.nix
@@ -1,6 +1,5 @@
 {pkgs}:
 pkgs.writeShellScriptBin "autogit" ''
-  echo "Syncing with git"
   ${pkgs.git}/bin/git add .
   read -p 'Commit Message: ' commitmsg
   ${pkgs.git}/bin/git commit -m "$commitmsg"
